99f05f49599cc1f54bbf7b356c1c2b9d295cb040
henrycrem <christophertokpah6@gmail.com>
2025-08-04 16:48:41 +0000

    dynamic added

--- app/setPassword/page.tsx
+++ app/setPassword/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from "react"
+
 import SetPasswordForm from '@/components/setPassword'
 import React from 'react'
+
+
+
+export const dynamic = "force-dynamic"
+
 
 const SetPassword = () => {
   return (
