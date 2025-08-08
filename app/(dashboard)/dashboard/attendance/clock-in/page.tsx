77bb7c3966e362a7d90db6a0e92a48a83e6193ee
henrycrem <christophertokpah6@gmail.com>
2025-08-08 16:59:31 +0000

    add sever implementation

--- app/(dashboard)/dashboard/attendance/clock-in/page.tsx
+++ app/(dashboard)/dashboard/attendance/clock-in/page.tsx
@@ -2,6 +2,8 @@ import SimpleAttendanceClock from "@/components/clock-in"
 import { getCurrentUserAction } from "@/actions/auth"
 import { redirect } from "next/navigation"
 
+export const dynamic = "force-dynamic"
+
 export default async function AttendanceClockPage() {
   let user = null
   
