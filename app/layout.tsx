047d697072b7c8af98091e8643f589ad8e6fb08d
henrycrem <christophertokpah6@gmail.com>
2025-12-11 13:07:44 +0000

    new implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/providers/providers";
 
+// my font
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
